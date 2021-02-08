5b764019e65b0c181b9e55fed185dc95cea3a55e
Brian Kozub <brikozub@gmail.com>
2021-02-07 20:32:19 -0700

    Schedule functionality working.

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import tweepy
 import os
 from dotenv import load_dotenv
+import schedule
+import time
 
 load_dotenv()
 
@@ -15,5 +17,18 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 # Beginning Functionality #
-public_tweets = api.home_timeline()
-print (public_tweets[0]._json['user']['screen_name'])
+
+def print_user():
+    public_tweets = api.home_timeline()
+    print (public_tweets[0]._json['user']['screen_name'])
+
+# for tweet in public_tweets:
+#     if tweet._json['user']['screen_name'] == "AviiWorkspace" and not tweet._json['favorited']:
+#         api.create_favorite(tweet._json['id'])
+#         api.retweet(tweet._json['id'])
+
+schedule.every(5).seconds.do(print_user)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
